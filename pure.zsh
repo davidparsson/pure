ddf3c4a1e1e8e11d7ed95370965a1ec6e2c39a09
David Pärsson <david@parsson.se>
2019-03-04 11:34:08 +0100

    Don't show CI check when hub is not installed

--- pure.zsh
+++ pure.zsh
@@ -314,7 +314,7 @@ prompt_pure_async_git_arrows() {
 prompt_pure_async_ci_status() {
 	setopt localoptions noshwordsplit
 	builtin cd -q $1
-	builtin type hub &> /dev/null && command hub ci-status
+	builtin type hub &> /dev/null && command hub ci-status || (exit 3)
 }
 
 prompt_pure_async_pyenv_version() {
