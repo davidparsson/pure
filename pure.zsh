c15c9c21c2db46d140fbbd9d4b54b35472ee1a52
David Pärsson <david@parsson.se>
2019-05-15 14:12:14 +0200

    Fix broken hub existance check

--- pure.zsh
+++ pure.zsh
@@ -314,7 +314,7 @@ prompt_pure_async_git_arrows() {
 prompt_pure_async_ci_status() {
 	setopt localoptions noshwordsplit
 	builtin cd -q $1
-	builtin type hub &> /dev/null && command hub ci-status || (exit 3)
+	(builtin type hub &> /dev/null || builtin exit 3) && command hub ci-status
 }
 
 prompt_pure_async_pyenv_version() {
